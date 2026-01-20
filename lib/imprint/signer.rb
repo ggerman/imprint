d6d4b33fafdbd305b7bd14d65d45f5670dcbf89b
Giménez Silva Germén Alberto <ggerman@gmail.com>
2026-01-20 13:05:08 -0300

    Fix indentation for secure_compare to satisfy RuboCop

--- lib/imprint/signer.rb
+++ lib/imprint/signer.rb
@@ -39,7 +39,7 @@ module Imprint
     def self.secure_compare(a, b)
       return false unless a.bytesize == b.bytesize
 
-      ActiveSupport::SecurityUtils.secure_compare(a, b)
+      OpenSSL.fixed_length_secure_compare(a, b)
     end
   end
 end
