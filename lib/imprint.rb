b507eee5dea9ec9bde96fbc5906360d92aa15872
Giménez Silva Germén Alberto <ggerman@gmail.com>
2026-01-20 12:56:30 -0300

    Add token verification with expiration handling

--- lib/imprint.rb
+++ lib/imprint.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require "imprint/version"
-require "imprint/signer"
+require 'imprint/version'
+require 'imprint/signer'
 
 module Imprint
   def self.sign(source:, watermark:, expires_in:)
@@ -11,15 +11,25 @@ module Imprint
       exp: expires_in.from_now.to_i
     }
 
-    token = Signer.sign(
-      payload,
-      secret: secret_key
-    )
-
+    token = Signer.sign(payload, secret: secret_key)
     "/imprint/render/#{token}"
   end
 
+  def self.verify(token)
+    payload = Signer.verify(token, secret: secret_key)
+    return nil unless payload.is_a?(Hash)
+
+    return nil if payload['exp'].to_i < Time.now.to_i
+
+    payload
+  end
+
   def self.secret_key
-    Rails.application.secret_key_base
+    # sin engine todavía; Rails solo si existe
+    if defined?(Rails)
+      Rails.application.secret_key_base
+    else
+      ENV['IMPRINT_SECRET'] || 'imprint-dev-secret'
+    end
   end
 end
