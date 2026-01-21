3d1cf7f45a8dbd6d51ab44a06f04a1031fc6893d
Giménez Silva Germén Alberto <ggerman@gmail.com>
2026-01-21 08:59:51 -0300

    Fix Rails helper indentation for RuboCop

--- lib/imprint/rails/helper.rb
+++ lib/imprint/rails/helper.rb
@@ -3,12 +3,12 @@
 module Imprint
   module Rails
     module Helper
-        def imprint_image_tag(token, **)
-          image_tag(
-              imprint_render_path(token: token),
-              **
-          )
-        end
+      def imprint_image_tag(token, **)
+        image_tag(
+          imprint_render_path(token: token),
+          **
+        )
+      end
     end
   end
 end
